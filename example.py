5e7d8a2ec77f1888764e945c68fd714000cd00ff
mainlyjada <jadak.sinclair001@gmail.com>
2024-10-01 11:49:15 -0400

    Add files via upload

---
+++ example.py
@@ -0,0 +1,13 @@
+# In- class dictionary examples
+# Dictionary hold key : value pairs
+
+# Create a dictionary
+persosn1 = {"name" : "Dylan" , "age" : 23, "hieght" : 50 . 2}
+
+# Get values from dictionary
+print(person1["age"])
+print ()
+print(f"The student's name is {person1 [ 'name' ] } " )
+
+# Add a key:value pair to the dictionary
+person1 
